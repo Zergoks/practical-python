f16ac3a9c774952ef59b84cf6740c3eef05548eb
Anton Ladanev <anton.ladanev@gmail.com>
2020-10-23 21:01:20 +0300

    1.27 solved

--- Work/pcost.py
+++ Work/pcost.py
@@ -1,3 +1,15 @@
 # pcost.py
 #
 # Exercise 1.27
+file_path = './Data/portfolio.csv'
+result = 0
+
+with open(file_path, 'rt') as file:
+    for line in file:
+        try:
+            data = line.split(',')
+            result += int(data[1]) * float(data[2])
+        except ValueError:
+            pass
+print(result)
+
